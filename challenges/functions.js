4e0ee03ee0492437a4e5b2788458cd3bee8b3c9e
Clif <clifhodges13@gmail.com>
2019-07-13 12:04:57 -0500

    Object-arrays complete. caught up on .filter. Functions complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(param1, param2, cb) {
+  console.log(cb(param1, param2))
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +17,23 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add = (a, b) => {
+  return a + b
+}
+
+const multiply = (a, b) => {
+  return a * b
+}
+
+const greeting = (firstName, lastName) => {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +41,8 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+
+// nestedFuntion() is inside of myFunction() and therefore has access to it's surrounding environment. Closure tells us that functions have access to their outside environments, but cannot reach in.
 
 
 const external = "I'm outside the function";
